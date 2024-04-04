2f807e5b25c67ec93ef08ff0694daf9b7862d6b4
Giulio Cocconi <coccogiulio8@gmail.com>
2024-04-04 18:09:13 +0200

    .

--- hardware-config.nix
+++ hardware-config.nix
@@ -29,9 +29,9 @@
         };
       };
     };
-    
-    boot.initrd.availableKernelModules = [ "ata_piix" "mptspi" "uhci_hcd" "ehci_pci" "sd_mod" "sr_mod" ];
-    boot.initrd.kernelModules = [ ];
-    boot.kernelModules = [ ];
-    boot.extraModulePackages = [ ];
-  }
+  }; 
+  boot.initrd.availableKernelModules = [ "ata_piix" "mptspi" "uhci_hcd" "ehci_pci" "sd_mod" "sr_mod" ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ ];
+  boot.extraModulePackages = [ ];
+}
